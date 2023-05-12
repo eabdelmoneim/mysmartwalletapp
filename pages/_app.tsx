a877b70404e2c6a513759b1d4dfab53f154f45d8
Eiman <eabdelmoneim@gmail.com>
2023-05-12 09:51:39 -0500

    added more wallet options on connect wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, paperWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
@@ -10,12 +10,21 @@ const activeChain = "mumbai";
 // load the API key and factory address from environment variables
 const thirdwebAPIKey = process.env.NEXT_PUBLIC_THIRDWEB_API_KEY as string;
 const factoryAddress = process.env.NEXT_PUBLIC_FACTORY_CONTRACT_ADDRESS as string;
+const paperWalletClientId = process.env.NEXT_PUBLIC_PAPER_KEY as string;
+const emailWallet = paperWallet({
+  clientId: paperWalletClientId
+});
+emailWallet.meta.name = "Email Wallet";
+emailWallet.meta.iconURL = "https://cdn0.iconfinder.com/data/icons/apple-apps/100/Apple_Mail-512.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}
     supportedWallets={[
-        smartWallet({
+      metamaskWallet(),
+      coinbaseWallet(),
+      emailWallet,
+     smartWallet({
           factoryAddress: factoryAddress, // Address of your account factory smart contract
           thirdwebApiKey: thirdwebAPIKey, // The API key you got from the previous step
           gasless: true,
